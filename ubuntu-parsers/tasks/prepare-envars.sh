21e3c62142972707292244c819241c6f60bcc0ac
Steve Greenberg <spgreenberg@gmail.com>
2019-11-14 09:06:08 -0700

    pass envars to docker build

--- ubuntu-parsers/tasks/prepare-envars.sh
+++ ubuntu-parsers/tasks/prepare-envars.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-cd jq-release
+jq_tag=$(cat jq-release/tag)
 
-jq_version=$(cat version)
-jq_tag=$(cat tag)
+yq_tag=$(cat yq-release/tag)
 
-echo "JQ Version: ${jq_version}"
-echo "JQ Tag: ${jq_tag}"
-
-cd ../yq-release
-
-yq_version=$(cat version)
-yq_tag=$(cat tag)
-
-echo "YQ Version: ${yq_version}"
-echo "YQ Tag: ${yq_tag}"
+echo '{ "JQ_VERSION": "'"${jq_tag}"'", "YQ_VERSION": "'"${yq_tag}"'" }' > envars/env.json

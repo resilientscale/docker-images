6eed30b41d60b117067076dfa4f40b9697e32f1a
Steve Greenberg <spgreenberg@gmail.com>
2020-01-29 09:38:07 -0700

    refactor for smaller images

--- set-pipelines.sh
+++ set-pipelines.sh
@@ -5,10 +5,18 @@ this_directory=$(dirname "$0")
 
 fly -t $target sp -p alpine-base -c ${this_directory}/alpine/pipeline.yml --non-interactive
 
-fly -t $target sp -p ubuntu-base -c ${this_directory}/ubuntu-base/pipeline.yml --non-interactive
-
 fly -t $target sp -p hugo -c ${this_directory}/docker/pipeline.yml --var image_name=hugo --var docker_file=${this_directory}/docker/hugo --non-interactive
 
 fly -t $target sp -p cf-cli -c ${this_directory}/docker/pipeline.yml --var image_name=cf-cli --var docker_file=${this_directory}/docker/cf-cli --non-interactive
 
-fly -t $target sp -p ubuntu-parsers -c ${this_directory}/ubuntu-parsers/pipeline.yml --non-interactive
+
+
+fly -t $target sp -p ubuntu-base -c ${this_directory}/ubuntu-base/pipeline.yml --non-interactive
+
+fly -t $target sp -p ubuntu-jq -c ${this_directory}/ubuntu-jq/pipeline.yml --non-interactive
+
+fly -t $target sp -p ubuntu-yq -c ${this_directory}/ubuntu-yq/pipeline.yml --non-interactive
+
+fly -t $target sp -p ubuntu-fly -c ${this_directory}/ubuntu-fly/pipeline.yml --non-interactive
+
+fly -t $target sp -p ubuntu-credhub -c ${this_directory}/ubuntu-credhub/pipeline.yml --non-interactive
--- ubuntu-parsers/tasks/prepare-envars.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-jq_tag=$(cat jq-release/tag)
-
-echo '{ "JQ_VERSION": "'"${jq_tag}"'" }' > envars/env.json

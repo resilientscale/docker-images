f37a5c6f42c60112de52e60eeda8c8eef46d8ef2
Steve Greenberg <spgreenberg@gmail.com>
2020-08-02 16:34:08 -0600

    remove comments

--- ubuntu-jq/tasks/prepare-envars.sh
+++ ubuntu-jq/tasks/prepare-envars.sh
@@ -4,10 +4,4 @@ jq_tag=$(cat jq-release/tag)
 
 echo '{ "JQ_VERSION": "'"${jq_tag}"'" }' > envars/env.json
 
-# tag="${jq_tag//jq-}"
-
-echo "${jq_tag}" > tag_file/tag
-
-# echo "envars" cat envars/env.json
-
-# echo "tag_file/tag: ${tag}" 
+echo "${jq_tag}" > tag_file/tag 

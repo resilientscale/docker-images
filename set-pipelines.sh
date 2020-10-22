2712d613ac4f7e3e67c81d953ac243ced9ab8fe5
Steve Greenberg <spgreenberg@gmail.com>
2020-10-22 14:08:37 -0600

    add docker image

--- set-pipelines.sh
+++ set-pipelines.sh
@@ -43,3 +43,5 @@ fly -t $target sp -p ubuntu-gcp-service-broker -c ${this_directory}/ubuntu-with-
 fly -t $target sp -p ubuntu-cloud-service-broker -c ${this_directory}/ubuntu-with-github-release/pipeline.yml -l ${this_directory}/ubuntu-cloud-service-broker/config.yml --non-interactive
 
 fly -t $target sp -p ubuntu-make -c ${this_directory}/ubuntu-with-git-tags/pipeline.yml -l ${this_directory}/ubuntu-make/config.yml --non-interactive
+
+fly -t $target sp -p ubuntu-docker -c ${this_directory}/ubuntu-with-github-release/pipeline.yml -l ${this_directory}/ubuntu-docker/config.yml --non-interactive

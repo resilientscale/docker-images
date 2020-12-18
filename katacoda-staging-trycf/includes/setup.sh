99bed4266a21ea1d58b34c042d364492a94551d9
Steve Greenberg <spgreenberg@gmail.com>
2020-12-18 12:45:48 -0700

    cf cli v7 echos 'failed' when not logged in

--- katacoda-staging-trycf/includes/setup.sh
+++ katacoda-staging-trycf/includes/setup.sh
@@ -5,8 +5,8 @@ rm -fr /home/tutorial/.cf-login.sh
 
 clear
 
-msg=$(cf t)
-if [ "$msg" == *"Not logged in."* ] || [ "$msg" == *"No API endpoint set"* ]; then
+msg=$(cf t | awk '{print tolower($0)}')
+if [ "$msg" == "failed" ]; then
 	echo "Provisioning failed. Please refresh this page to try again."
 else
 	echo "Provisioning complete. Please proceed with the tutorial."

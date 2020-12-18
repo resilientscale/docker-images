fc6e57bc8bbdca395e44ee0986035ba7c218cf5e
Steve Greenberg <spgreenberg@gmail.com>
2020-12-18 13:03:43 -0700

    don't remove the login file

--- katacoda-staging-trycf/includes/setup.sh
+++ katacoda-staging-trycf/includes/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 /home/tutorial/.cf-login.sh
-rm -fr /home/tutorial/.cf-login.sh
 
 clear
 
 msg=$(cf t | awk '{print tolower($0)}')
+echo " "
 if [ "$msg" == "failed" ]; then
 	echo "Provisioning failed. Please refresh this page to try again."
 else

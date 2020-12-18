f2ae93fe708c7815e254f7a56c9c7161d94663dc
Steve Greenberg <spgreenberg@gmail.com>
2020-12-18 11:51:30 -0700

    handle case where api endpoint is not set

--- katacoda-staging-trycf/setup.sh
+++ katacoda-staging-trycf/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 /home/tutorial/.cf-login.sh
+rm -fr /home/tutorial/.cf-login.sh
 
 clear
 
 msg=$(cf t)
-if [ "$msg" == *"Not logged in."* ]; then
+if [ "$msg" == *"Not logged in."* ] || [ "$msg" == *"No API endpoint set"* ]; then
 	echo "Provisioning failed. Please refresh this page to try again."
 else
 	echo "Provisioning complete. Please proceed with the tutorial."
